40a2db154c31ff052934cb94877f809f7f3cc5ff
katisa-sa <ksanz@birt.eus>
2025-12-19 16:35:46 +0100

    commit cambio llave

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiKey: "87db2298447e4c6c8657c99e05f2aa49",
+  apiKey: "95f72f17192c44e5861827c824a05dce",
   apiUrl: "https://newsapi.org/v2"
 };
